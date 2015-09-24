48af4686a7b87225e6b3986c187c07065f0dac6a
Evgeniy Moskalenko <evg@evgmoskalenko.com>
2015-09-24 21:33:07 +0300

    change kata on codewars

---
+++ src/com/codewars/basicSubclassesAdamAndEve/BasicSubclassesAdamAndEve.java
@@ -0,0 +1,8 @@
+package com.codewars.basicSubclassesAdamAndEve;
+
+/**
+ * http://www.codewars.com/kata/52ffcfa4aff455b3c2000750/train/java
+ */
+public class BasicSubclassesAdamAndEve {
+
+}
--- src/com/codewars/simpleInteractiveInterpreter/SimpleInteractiveInterpreter.java
+++
@@ -1,8 +0,0 @@
-package com.codewars.simpleInteractiveInterpreter;
-
-/**
- * http://www.codewars.com/kata/52ffcfa4aff455b3c2000750/train/java
- */
-public class SimpleInteractiveInterpreter {
-
-}
