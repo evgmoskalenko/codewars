d5151038af785771d4bf7a4fe85c58b65fc407f2
Evgeniy Moskalenko <evg@evgmoskalenko.com>
2015-09-24 21:54:01 +0300

    The creation method must return an array of length 2 containing objects representing Adam and Eva. The first object in the array should be an instance of the class Man. The second should be an instance of the class Woman. Both objects have to be subclasses of Human. Your job is to implement the Human, Man and Woman classes.

--- src/com/codewars/kyu8/basicSubclassesAdamAndEve/BasicSubclassesAdamAndEve.java
+++ src/com/codewars/kyu8/basicSubclassesAdamAndEve/BasicSubclassesAdamAndEve.java
@@ -3,6 +3,18 @@ package com.codewars.kyu8.basicSubclassesAdamAndEve;
 /**
  * http://www.codewars.com/kata/basic-subclasses-adam-and-eve
  */
-public class BasicSubclassesAdamAndEve {
 
+public class BasicSubclassesAdamAndEve {
+        public static Human[] create() {
+            return new Human[]{
+                    new Man(), new Woman()
+            };
+        }
+    }
+    class Human{
+    }
+    class Man extends Human{
+    }
+    class Woman extends Human{
+    }
 }
